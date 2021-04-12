1294008b953cba3f17203a47ed90c93a33e5ec79
Jason <56404503+Sungmin-jin@users.noreply.github.com>
2021-04-11 19:00:06 -0700

    first commit

---
+++ UDP Asynchronous Chat/UDP Asynchronous Chat/UDPAsynchronousChatClient.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace UDP_Asynchronous_Chat
+{
+    public class UDPAsynchronousChatClient : UDPChatGeneral
+    {
+        Socket mSockBroadCastSender;
+        IPEndPoint mIPEPBroadcast;
+        IPEndPoint mIPEPLocal;
+        private EndPoint mChatServerEP;
+
+        public UDPAsynchronousChatClient(int _localPort, int _remotePort)
+        {
+            mIPEPBroadcast = new IPEndPoint(IPAddress.Broadcast, _remotePort);
+            mIPEPLocal = new IPEndPoint(IPAddress.Any, _localPort);
+
+            mSockBroadCastSender = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            mSockBroadCastSender.EnableBroadcast = true;
+        }
+
+        public void SendBroadcast(string strDataForBroadcast)
+        {
+            if (string.IsNullOrEmpty(strDataForBroadcast))
+            {
+                return;
+            }
+            try
+            {
+                if (!mSockBroadCastSender.IsBound)
+                {
+                    mSockBroadCastSender.Bind(mIPEPLocal);
+                }
+                SocketAsyncEventArgs saea = new SocketAsyncEventArgs();
+                var dataBytes = Encoding.ASCII.GetBytes(strDataForBroadcast);
+                saea.SetBuffer(dataBytes, 0, dataBytes.Length);
+                saea.RemoteEndPoint = mIPEPBroadcast;
+
+                saea.Completed += SendCompletedCallBack;
+
+
+                mSockBroadCastSender.SendToAsync(saea);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                throw;
+            }
+        }
+
+        private void SendCompletedCallBack(object sender, SocketAsyncEventArgs e)
+        {
+            Console.WriteLine($"Data send succesfully to: {e.RemoteEndPoint}");
+            if (Encoding.ASCII.GetString(e.Buffer).Equals("<DISCOVER>"))
+            {
+                ReceiveTextFromServer(expectedValue: "<CONFIRM>", IPEPReceiverLocal: mIPEPLocal);
+            }
+        }
+
+        private void ReceiveTextFromServer(string expectedValue, IPEndPoint IPEPReceiverLocal)
+        {
+            if (IPEPReceiverLocal == null)
+            {
+                Console.WriteLine("No IPEndpoint specified");
+            }
+
+            SocketAsyncEventArgs saeaSendConfirmation = new SocketAsyncEventArgs();
+            saeaSendConfirmation.SetBuffer(new byte[1024], 0, 1024);
+            saeaSendConfirmation.RemoteEndPoint = IPEPReceiverLocal;
+
+            saeaSendConfirmation.UserToken = expectedValue;
+            saeaSendConfirmation.Completed += ReceiveconfirmationCompleted;
+
+            mSockBroadCastSender.ReceiveFromAsync(saeaSendConfirmation);
+
+        }
+
+        private void ReceiveconfirmationCompleted(object sender, SocketAsyncEventArgs e)
+        {
+            if (e.BytesTransferred == 0)
+            {
+                Debug.WriteLine($"Zero bytes transferred, socket error: {e.SocketError}");
+                return;
+            }
+
+            var receivedText = Encoding.ASCII.GetString(e.Buffer, 0, e.BytesTransferred);
+            var expectedText = Convert.ToString(e.UserToken);
+
+            if (receivedText.Equals(expectedText))
+            {
+                Console.WriteLine($"Received confirmation from server. {e.RemoteEndPoint}");
+                OnRaisePrintStringEvent(new PrintStringEventArgs($"Received confirmation from server. {e.RemoteEndPoint}"));
+                mChatServerEP = e.RemoteEndPoint;
+                ReceiveTextFromServer(string.Empty, mChatServerEP as IPEndPoint);
+            }
+            else if(string.IsNullOrEmpty(expectedText) && !string.IsNullOrEmpty(receivedText))
+            {
+                Console.WriteLine($"Text received: {receivedText}");
+                OnRaisePrintStringEvent(new PrintStringEventArgs(($"Text received: {receivedText}")));
+                ReceiveTextFromServer(string.Empty, mChatServerEP as IPEndPoint);
+            }
+            else if (!string.IsNullOrEmpty(expectedText) && !receivedText.Equals(expectedText))
+            {
+                Console.WriteLine($"Expected token not returned by the server");
+                OnRaisePrintStringEvent(new PrintStringEventArgs(($"Expected token not returned by the server")));
+            }
+        }
+
+
+        public void SendMessageToKnownServer(string message)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(message))
+                {
+                    return;
+                }
+
+                var bytesToSend = Encoding.ASCII.GetBytes(message);
+
+                SocketAsyncEventArgs saea = new SocketAsyncEventArgs();
+                saea.SetBuffer(bytesToSend, 0, bytesToSend.Length);
+
+                saea.RemoteEndPoint = mChatServerEP;
+
+                saea.UserToken = message;
+
+                saea.Completed += SendMessageToKnownServerCompletedCallback;
+                mSockBroadCastSender.SendToAsync(saea);
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        private void SendMessageToKnownServerCompletedCallback(object sender, SocketAsyncEventArgs e)
+        {
+            Console.WriteLine($"Sent: {e.UserToken}{Environment.NewLine}Server: {e.RemoteEndPoint}");
+            OnRaisePrintStringEvent(new PrintStringEventArgs(($"Sent: {e.UserToken}{Environment.NewLine}Server: {e.RemoteEndPoint}")));
+
+
+        }
+    }
+
+  
+}
---
+++ UDP Asynchronous Chat/UDP Asynchronous Chat/UDPAsynchronousChatServer.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace UDP_Asynchronous_Chat
+{
+    public class UDPAsynchronousChatServer :  UDPChatGeneral
+    {
+        Socket mSockBroadcastReceiver;
+        IPEndPoint mIPEPLocal;
+        private int retryCount;
+
+        List<EndPoint> mListOfClients;
+        public UDPAsynchronousChatServer()
+        {
+            mSockBroadcastReceiver = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            mIPEPLocal = new IPEndPoint(IPAddress.Any, 23000);
+            mSockBroadcastReceiver.EnableBroadcast = true;
+            retryCount = 0;
+
+            mListOfClients = new List<EndPoint>();
+        }
+
+        public void StartReceivingData()
+        {
+            try
+            {
+                SocketAsyncEventArgs saea = new SocketAsyncEventArgs();
+                saea.SetBuffer(new byte[1024], 0, 1024);
+                saea.RemoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+                if (!mSockBroadcastReceiver.IsBound)
+                {
+                    mSockBroadcastReceiver.Bind(mIPEPLocal);
+                }
+                saea.Completed += ReceiveCompletedCallback;
+                if (!mSockBroadcastReceiver.ReceiveFromAsync(saea))
+                {
+                    Console.WriteLine($"Failed to receive data - socket error: {saea.SocketError}");
+                    OnRaisePrintStringEvent(new PrintStringEventArgs($"Failed to receive data - socket error: {saea.SocketError}"));
+                    if (retryCount++ >= 10)
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        StartReceivingData();
+                    }
+                    StartReceivingData();
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.ToString());
+                throw;
+            }
+        }
+
+        private void ReceiveCompletedCallback(object sender, SocketAsyncEventArgs e)
+        {
+            string textReceived = Encoding.ASCII.GetString(e.Buffer, 0, e.BytesTransferred);
+            Console.WriteLine(
+                $"Text received {textReceived}{Environment.NewLine}" +
+                $"Number of bytes received {e.BytesTransferred}{Environment.NewLine}" +
+                $"Received data from endpoint: {e.RemoteEndPoint}{Environment.NewLine}"
+                );
+            if (textReceived.Equals("<DISCOVER>"))
+            {
+                if (!mListOfClients.Contains(e.RemoteEndPoint))
+                {
+                    mListOfClients.Add(e.RemoteEndPoint);
+                    Console.WriteLine("Total clients: " + mListOfClients.Count);
+                    OnRaisePrintStringEvent(new PrintStringEventArgs(($"New client: {e.RemoteEndPoint} - Total clients: " + mListOfClients.Count)));
+                }
+
+                SendTextToEndPoint("<CONFIRM>", e.RemoteEndPoint);
+            }
+            else
+            {
+                foreach(IPEndPoint remEP in mListOfClients)
+                {
+                    if (!remEP.Equals(e.RemoteEndPoint))
+                    {
+                        SendTextToEndPoint(textReceived, remEP);
+                    }
+                }
+            }
+            StartReceivingData();
+        }
+
+        private void SendTextToEndPoint(string textToSend, EndPoint remoteEndPoint)
+        {
+            if(string.IsNullOrEmpty(textToSend) || remoteEndPoint == null)
+            {
+                return;
+            }
+            SocketAsyncEventArgs saeaSend = new SocketAsyncEventArgs();
+            saeaSend.RemoteEndPoint = remoteEndPoint;
+            var bytesToSend = Encoding.ASCII.GetBytes(textToSend);
+
+            saeaSend.SetBuffer(bytesToSend, 0, bytesToSend.Length);
+
+            saeaSend.Completed += SendTextToEndPointCompleted;
+            mSockBroadcastReceiver.SendToAsync(saeaSend);
+        }
+
+        private void SendTextToEndPointCompleted(object sender, SocketAsyncEventArgs e)
+        {
+            Console.WriteLine($"Completed sending text to {e.RemoteEndPoint}");
+        }
+    }
+}
